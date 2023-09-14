9f339eff265b5fa7710fa49fbb2c5161e585c3bd
Bruna Gorgen <gorgenbruna@gmail.com>
2023-09-14 12:05:48 +0100

    projeto de estudos todo-list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+
+import './App.css'
+
+import Filter from './components/Filter'
+import Search from './components/Search'
+import Todo from './components/Todo'
+import TodoForm from './components/TodoForm'
+
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'Criar funcionalidade X no sistema',
+      category: 'Trabalho',
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: 'Ir para a cademia',
+      category: 'Pessoal',
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: 'Estudar React',
+      category: 'Estudos',
+      isCompleted: false,
+    },
+  ])
+
+  const [search, setSearch] = useState('') 
+  const [filter, setFilter] = useState('All')
+  const [sort, setSort] = useState('Asc')
+
+  const addTodo = (text, category) => {
+
+    const newTodos = [...todos, {
+      id: Math.floor(Math.random() * 10000),
+      text,
+      category,
+      isCompleted: false,
+    }]
+
+    setTodos(newTodos)
+  }
+
+  const removeTodo = (id) => {
+    const newTodos = [...todos]
+    const filteredTodos = newTodos.filter(todo => 
+      todo.id !== id ? todo : null
+      )  
+    setTodos(filteredTodos)
+  }
+
+  const completeTodo = (id) => { 
+    const newTodos = [...todos]
+    newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo)
+    setTodos(newTodos)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>Lista de Tarefas</h1>
+      <Search search={search} setSearch={setSearch} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
+      <div className='todo-list'>
+        {todos
+          .filter((todo) => 
+          filter === 'All' ? true : filter === 'Completed' ? todo.isCompleted : !todo.isCompleted)
+          .filter((todo) =>
+            todo.text.toLowerCase().includes(search.toLowerCase())
+            )
+          .sort((a, b) => sort === 'Asc' ? a.text.localeCompare(b.text) :  b.text.localeCompare(a.text))
+          .map((todo) => (
+          <Todo 
+          key={todo.id} 
+          todo={todo} 
+          removeTodo={removeTodo} 
+          completeTodo={completeTodo}
+          />
+        ))}
+      </div>
+      <TodoForm addTodo= {addTodo}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
